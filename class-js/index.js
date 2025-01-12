adba47263a26f0f807981abf02dfb2120207ec55
SirfTum <nomy.hope07@gmail.com>
2025-01-12 11:52:22 +0500

    this is my first course on 12.01.2024

---
+++ class-js/index.js
@@ -0,0 +1,123 @@
+// console.log("hello world");
+// for print we run this commend
+
+// alert("hello world");
+
+// alert show message on web page
+
+
+// variables  three types of variables 
+
+// var = 
+
+// const = fixed
+
+// let =     used in function 
+// let x = 5;
+// let y = 2;
+// let total;
+// total = x + y;
+// console.log(total)
+
+////// DATA TYPES///////
+// interger, 123456789 numbring
+//  float, decimal values
+
+//  array , [1,2,"hamza", {}, "ali"]
+//  object {key , values pair}
+
+// var arr = ["1", 2, "hello",{}];
+// console.log(arr)
+
+// var arr = [1,2,3,4,5,6,7,8,];
+// arr.push(9); // add value in end of array
+// arr.pop(); // remove value from end
+// arr.shift(); //remove value first value from start and from the end of the list
+// arr.unshift(1);// add value in start of the list
+
+// var index = arr.indexOf(3); // show the value of index
+// var sliceData = arr.slice(index,index=2)// how cut of value
+// console.log(sliceData)
+
+
+// // var x = 5 ;
+
+// // x = 3
+
+// // console.log(x);
+
+
+// var num = 2.8;
+
+// console.log(Math.floor(num))      // shows smalest value
+// console.log(Math.round(num))      // shows round Off value bigger than .5 or less than .5
+// console.log(Math.ceil(num))      // shows biggest value
+
+
+
+
+
+
+// console.log(Math.random())         //    shows random value from computer generated 
+
+
+
+// var name1="Aqeel";
+// var name2="SirfTum"
+// var randomRation = Math.random()*100;
+
+// if (randomRation>50){
+//     console.log("Aqeel ❤ SirfTum");
+// }
+// else {
+//     console.log("SirfTum does True loves")
+// }
+// console.log()
+
+// // NOW WE TAKE INPUT FROM USER 
+// var name1 = prompt("Enter Your Name");
+// var name2 = prompt("Enter your Crush Name");
+// var randomRatio = Math.random()*100;
+
+// if (randomRatio>50){
+//     console.log(name1 +" 💕" +name2);
+// }
+
+
+
+
+
+// var name1 = prompt("Enter Your Name");
+// var name2 = prompt("Enter your Crush Name");
+// var randomRatio = Math.random()*100;
+// var img = [
+//     "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.informalnewz.com%2Fpakistani-actress-haniya-amir-became-a-bride-wearing-a-transparent-saree-shared-the-latest-video-11420237812%2F&psig=AOvVaw1tM5GWtEiPz95ksRa6WVah&ust=1736150250901000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCJiGmeqN3ooDFQAAAAAdAAAAABAE",
+//     "https://www.google.com/url?sa=i&url=https%3A%2F%2Fen.wikipedia.org%2Fwiki%2FShakira&psig=AOvVaw3FMGHLcecKexWenpuw_G_n&ust=1736150328604000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCKjE1o-O3ooDFQAAAAAdAAAAABAE"
+// ];
+// var heartImages = [
+//     "https://th.bing.com/th/id/OIP.CDCRc2-r33x0SdiT0Htx3IwHaGc?rs=11&pid=ImgDetMain",
+//     "https://th.bing.com/th/id/OIP.yP-VIJ2VWnSCHI13qG--QHaHa?rs=1&pid=ImgDetMain"
+// ]
+
+
+
+// var pic1 = document.getElementById("name1");
+// var pic2 = document.getElementById("name2");
+// var heartPicture = document.getElementById("heart")
+
+// pic1.src = images[0];
+// pic2.src = images[1];
+
+// if (randomRadio > 50){
+//     // console.log(name1 + "❤" + name2);
+    
+    
+// }else {
+//     console.log(name2 + "too much loves " + name1 );
+// }
+
+
+var num1 = 1;
+var num2 = 1;
+var total = num1 + num2;
+console.log(total)
